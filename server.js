0c04fc308e230a2985a0b2f9231c40276ff0bc6d
yash5chandrakar <yash5chandrakar@gmail.com>
2025-04-26 17:39:38 +0530

    server changes

--- server.js
+++ server.js
@@ -31,6 +31,8 @@ app.get("/", (req, res) => {
 
 app.use("/api", routes);
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
